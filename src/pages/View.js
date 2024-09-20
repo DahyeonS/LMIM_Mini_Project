e25121fa29bbde1598e291f9e85249e8fb078f6f
Dahyeon Seo <siren199704@gmail.com>
2024-09-21 04:08:39 +0900

    링크 복사 기능 추가

--- src/pages/View.js
+++ src/pages/View.js
@@ -79,6 +79,12 @@ export default function View() {
     // 입력값 처리 부분
     const [csrfToken] = useCsrfToken();
 
+    // 링크 복사
+    const handleCopyLink = () => {
+        navigator.clipboard.writeText(`${window.location.href}?num=${index}`);
+        alert('주소가 복사되었습니다.');
+    }
+
     // 게시물 삭제
     const handleDelete = (e) => {
         e.preventDefault(); // 페이지 변경 방지
@@ -124,12 +130,22 @@ export default function View() {
                                     </div>
                                     <div className='mt-2'>
                                         <Link className='btn btn-primary' to={'/post'}>목록보기</Link>
+                                        <button onClick={handleCopyLink} className='btn btn-outline-secondary ms-2'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor" className='mb-1'>
+                                                <path d="M17.6567 14.8284L16.2425 13.4142L17.6567 12C19.2188 10.4379 19.2188 7.90524 17.6567 6.34314C16.0946 4.78105 13.5619 4.78105 11.9998 6.34314L10.5856 7.75736L9.17139 6.34314L10.5856 4.92893C12.9287 2.58578 16.7277 2.58578 19.0709 4.92893C21.414 7.27208 21.414 11.0711 19.0709 13.4142L17.6567 14.8284ZM14.8282 17.6569L13.414 19.0711C11.0709 21.4142 7.27189 21.4142 4.92875 19.0711C2.5856 16.7279 2.5856 12.9289 4.92875 10.5858L6.34296 9.17157L7.75717 10.5858L6.34296 12C4.78086 13.5621 4.78086 16.0948 6.34296 17.6569C7.90506 19.2189 10.4377 19.2189 11.9998 17.6569L13.414 16.2426L14.8282 17.6569ZM14.8282 7.75736L16.2425 9.17157L9.17139 16.2426L7.75717 14.8284L14.8282 7.75736Z"/>
+                                            </svg>
+                                        </button>
                                     </div>
                                 </Fragment>
                             : // 데스크톱
                                 <Fragment>
                                 <div className='col-2'>
                                     <Link className='btn btn-primary' to={'/post'}>목록보기</Link>
+                                    <button onClick={handleCopyLink} className='btn btn-outline-secondary ms-2'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor" className='mb-1'>
+                                            <path d="M17.6567 14.8284L16.2425 13.4142L17.6567 12C19.2188 10.4379 19.2188 7.90524 17.6567 6.34314C16.0946 4.78105 13.5619 4.78105 11.9998 6.34314L10.5856 7.75736L9.17139 6.34314L10.5856 4.92893C12.9287 2.58578 16.7277 2.58578 19.0709 4.92893C21.414 7.27208 21.414 11.0711 19.0709 13.4142L17.6567 14.8284ZM14.8282 17.6569L13.414 19.0711C11.0709 21.4142 7.27189 21.4142 4.92875 19.0711C2.5856 16.7279 2.5856 12.9289 4.92875 10.5858L6.34296 9.17157L7.75717 10.5858L6.34296 12C4.78086 13.5621 4.78086 16.0948 6.34296 17.6569C7.90506 19.2189 10.4377 19.2189 11.9998 17.6569L13.414 16.2426L14.8282 17.6569ZM14.8282 7.75736L16.2425 9.17157L9.17139 16.2426L7.75717 14.8284L14.8282 7.75736Z"/>
+                                        </svg>
+                                    </button>
                                 </div>
                                 <div className='col-6 col-lg-7'/>
                                 <div className='col-4 col-lg-3'>
@@ -141,8 +157,13 @@ export default function View() {
                         </div>
                     ) : ( // 비로그인
                         <div className={`row ${!isMobile ? 'pt-2' : 'pt-1'}`}>
-                            <div className='col-7 col-md-10'/>
-                            <div className='col-5 col-md-2'>
+                            <div className='col-5 col-lg-10'/>
+                            <div className='col-7 col-lg-2'>
+                                <button onClick={handleCopyLink} className='btn btn-outline-secondary ms-2 float-end'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor" className='mb-1'>
+                                        <path d="M17.6567 14.8284L16.2425 13.4142L17.6567 12C19.2188 10.4379 19.2188 7.90524 17.6567 6.34314C16.0946 4.78105 13.5619 4.78105 11.9998 6.34314L10.5856 7.75736L9.17139 6.34314L10.5856 4.92893C12.9287 2.58578 16.7277 2.58578 19.0709 4.92893C21.414 7.27208 21.414 11.0711 19.0709 13.4142L17.6567 14.8284ZM14.8282 17.6569L13.414 19.0711C11.0709 21.4142 7.27189 21.4142 4.92875 19.0711C2.5856 16.7279 2.5856 12.9289 4.92875 10.5858L6.34296 9.17157L7.75717 10.5858L6.34296 12C4.78086 13.5621 4.78086 16.0948 6.34296 17.6569C7.90506 19.2189 10.4377 19.2189 11.9998 17.6569L13.414 16.2426L14.8282 17.6569ZM14.8282 7.75736L16.2425 9.17157L9.17139 16.2426L7.75717 14.8284L14.8282 7.75736Z"/>
+                                    </svg>
+                                </button>
                                 <Link className='btn btn-primary float-end' to={'/post'}>목록보기</Link>
                             </div>
                         </div>
