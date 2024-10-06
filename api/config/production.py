c60a521ff09250d810caf7991f00c3298c471b72
Dahyeon Seo <siren199704@gmail.com>
2024-10-07 01:29:58 +0900

    Update production.py

--- api/config/production.py
+++ api/config/production.py
@@ -7,7 +7,7 @@ pw = os.getenv('DB_PASSWORD') # 암호
 url = os.getenv('DB_HOST') # 데이터베이스 주소
 db = os.getenv('DB_NAME') # 데이터베이스 이름
 
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{pw}@{url}/{db}'
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{pw}@{url}:{'port':3306}/{db}?charset=utf8"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b64decode(os.getenv('SECRET_KEY')) # 시크릿 키
 JWT_SECRET_KEY = b64decode(os.getenv('JWT_SECRET_KEY')) # JWT 시크릿 키
@@ -24,7 +24,7 @@ dictConfig({
         'file': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/calendar.log'),
+            'filename': os.path.join(BASE_DIR, 'logs/lmim_project.log'),
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'default',
