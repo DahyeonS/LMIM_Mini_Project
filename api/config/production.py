3debfe45288798e43152b0b163062104aeb7f352
Dahyeon Seo <siren199704@gmail.com>
2024-10-08 00:02:40 +0900

    Update production.py

--- api/config/production.py
+++ api/config/production.py
@@ -7,7 +7,7 @@ url = os.getenv('DB_HOST') # 데이터베이스 주소
 db = os.getenv('DB_NAME') # 데이터베이스 이름
 
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{pw}@{url}:{'port':3306}/{db}?charset=utf8"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{pw}@{url}:3306/{db}?charset=utf8"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b64decode(os.getenv('SECRET_KEY')) # 시크릿 키
 JWT_SECRET_KEY = b64decode(os.getenv('JWT_SECRET_KEY')) # JWT 시크릿 키
