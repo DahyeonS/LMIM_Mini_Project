7efe02c9a7e1ae5e68f955d4b34ef854e1b58d31
Dahyeon Seo <siren199704@gmail.com>
2024-10-07 23:00:17 +0900

    Update production.py

--- api/config/production.py
+++ api/config/production.py
@@ -1,5 +1,4 @@
 from config.default import *
-from logging.config import dictConfig
 from base64 import b64decode
 
 user = os.getenv('DB_USER') # 사용자 이름
@@ -13,26 +12,3 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b64decode(os.getenv('SECRET_KEY')) # 시크릿 키
 JWT_SECRET_KEY = b64decode(os.getenv('JWT_SECRET_KEY')) # JWT 시크릿 키
 CORS_RESOURCES = {r'/*': {'origins': ''}} # CORS 허용 주소
-
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-        }
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/lmim_project.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'default',
-        },
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['file']
-    }
-})
