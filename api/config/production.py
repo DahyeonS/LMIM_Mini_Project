1a982a1a7454015dda2f0239e039af0cf0d6a4c8
Dahyeon Seo <siren199704@gmail.com>
2024-10-10 01:52:02 +0900

    Update production.py

--- api/config/production.py
+++ api/config/production.py
@@ -7,8 +7,9 @@ url = os.getenv('DB_HOST') # 데이터베이스 주소
 db = os.getenv('DB_NAME') # 데이터베이스 이름
 
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{pw}@{url}:3306/{db}?charset=utf8"
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://{user}:{pw}@{url}/{db}?charset=utf8"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle' : 280}
 SECRET_KEY = b64decode(os.getenv('SECRET_KEY')) # 시크릿 키
 JWT_SECRET_KEY = b64decode(os.getenv('JWT_SECRET_KEY')) # JWT 시크릿 키
 CORS_RESOURCES = {r'/*': {'origins': 'https://dahyeonseo.pythonanywhere.com/'}} # CORS 허용 주소
