eaad0f47a42cf6db11fae750732685820a2652f3
Dahyeon Seo <siren199704@gmail.com>
2024-12-17 00:57:29 +0900

    링크드인 아이콘 추가

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../index.css'
+import '../index.css';
+import { Link } from 'react-router-dom';
 
 export default function About() {
     return (
@@ -9,7 +10,7 @@ export default function About() {
             </div>
             <div className='pb-5 mt-4'>
                 <ul className='fs-4'>
-                    <li><h4><span className='fw-bold me-3'>이름</span>서 다현</h4></li>
+                    <li><h4><span className='fw-bold me-3'>이름</span>서 다현 <Link className='text-secondary ms-1' to={'https://www.linkedin.com/in/dahyeon-seo-53b101342/'} target='_blank'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="currentColor"><path d="M18.3362 18.339H15.6707V14.1622C15.6707 13.1662 15.6505 11.8845 14.2817 11.8845C12.892 11.8845 12.6797 12.9683 12.6797 14.0887V18.339H10.0142V9.75H12.5747V10.9207H12.6092C12.967 10.2457 13.837 9.53325 15.1367 9.53325C17.8375 9.53325 18.337 11.3108 18.337 13.6245V18.339H18.3362ZM7.00373 8.57475C6.14573 8.57475 5.45648 7.88025 5.45648 7.026C5.45648 6.1725 6.14648 5.47875 7.00373 5.47875C7.85873 5.47875 8.55173 6.1725 8.55173 7.026C8.55173 7.88025 7.85798 8.57475 7.00373 8.57475ZM8.34023 18.339H5.66723V9.75H8.34023V18.339ZM19.6697 3H4.32923C3.59498 3 3.00098 3.5805 3.00098 4.29675V19.7033C3.00098 20.4202 3.59498 21 4.32923 21H19.6675C20.401 21 21.001 20.4202 21.001 19.7033V4.29675C21.001 3.5805 20.401 3 19.6675 3H19.6697Z"/></svg></Link></h4></li>
                     <li>
                         <h4 className='fw-bold'>경력 사항</h4>
                         <ul className='mb-3'>
